66d988a43341dedbb0ae11278b584a03c71121f4
Rajesh <rajeshkhandekar912@gmail.com>
2021-06-27 13:26:42 +0530

    m

---
+++ C++/3_variables_comments.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+//This program is created by webruster_.
+int main(){
+    
+    cout<< "Don't add a programming language to your skills list by only watching one hour tutorial ";
+    return 0;
+}
